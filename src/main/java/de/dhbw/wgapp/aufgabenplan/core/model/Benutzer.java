3ce493f5ab32e8ba6032c727bd8dab8ed35dc507
Ralf Klemmer <r_klemmer@gmx.de>
2022-03-18 09:52:12 +0100

    DomainModel erstellt

---
+++ src/main/java/de/dhbw/wgapp/aufgabenplan/core/model/Benutzer.java
@@ -0,0 +1,6 @@
+package de.dhbw.wgapp.aufgabenplan.core.model;
+
+public class Benutzer {
+    String name;
+    String kontoverbindung;
+}
---
+++ src/main/java/de/dhbw/wgapp/aufgabenplan/core/model/Task.java
@@ -0,0 +1,10 @@
+package de.dhbw.wgapp.aufgabenplan.core.model;
+
+import java.time.LocalDate;
+
+public class Task {
+    public String titel;
+    public String bearbeiter;
+    // Ausführungsdatum
+    public LocalDate dueDay;
+}
